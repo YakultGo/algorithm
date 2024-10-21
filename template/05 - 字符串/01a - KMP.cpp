907db1c59555d12c716a4e75811d7e38a69d5b80
YakultGo <god1239426256@163.com>
2024-10-21 20:23:18 +0800

    修改KMP中的变量名，更符合直觉

--- template/05 - 字符串/01a - KMP.cpp
+++ template/05 - 字符串/01a - KMP.cpp
@@ -1,25 +1,25 @@
 struct KMP {
-    vector<int> nex(string &s) {
+    vector<int> prefix(string &s) {
         int n = s.size();
-        vector<int> pi(n);
+        vector<int> nex(n);
         for (int i = 1; i < n; i++) {
-            int j = pi[i - 1];
-            while (j > 0 && s[i] != s[j]) j = pi[j - 1];
+            int j = nex[i - 1];
+            while (j > 0 && s[i] != s[j]) j = nex[j - 1];
             if (s[i] == s[j]) j++;
-            pi[i] = j;
+            nex[i] = j;
         }
-        return pi;
+        return nex;
     }
     vector<int> work(string &text, string &pattern) {
-        vector<int> pi = nex(pattern);
+        vector<int> nex = prefix(pattern);
         vector<int> res;
         int j = 0, m = pattern.size();
         for (int i = 0; i < text.size(); i++) {
-            while (j > 0 && text[i] != pattern[j]) j = pi[j - 1];
+            while (j > 0 && text[i] != pattern[j]) j = nex[j - 1];
             if (text[i] == pattern[j]) j++;
             if (j == m) {
                 res.push_back(i - m + 1);
-                j = pi[j - 1];
+                j = nex[j - 1];
             }
         }
         return res;
