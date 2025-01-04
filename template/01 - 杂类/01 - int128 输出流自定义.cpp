938ed846109eb4708fc9ff7971e5ca6df1c4ba65
YakultGo <god1239426256@163.com>
2025-01-04 15:55:38 +0800

    update

--- template/01 - 杂类/01 - int128 输出流自定义.cpp
+++ template/01 - 杂类/01 - int128 输出流自定义.cpp
@@ -2,6 +2,13 @@ using i128 = __int128;
 
 std::ostream &operator<<(std::ostream &os, i128 n) {
     std::string s;
+    if (n < 0) {
+        os << '-';
+        n = -n;
+    }
+    if(n == 0) {
+        return os << '0';
+    }
     while (n) {
         s += '0' + n % 10;
         n /= 10;
--- template/04 - 数据结构/01 - 树状数组.cpp
+++ template/04 - 数据结构/01 - 树状数组.cpp
@@ -7,6 +7,7 @@ struct BIT {
         return x & (-x);
     }
     void add(int x, T delta) {
+        if (x == 0) return;
         while (x < tr.size()) {
             tr[x] += delta;
             x += lowbit(x);
@@ -21,6 +22,7 @@ struct BIT {
         return res;
     }
     T calc(int l, int r) {
+        if (l > r) return T(0);
         return query(r) - query(l - 1);
     }
 };
